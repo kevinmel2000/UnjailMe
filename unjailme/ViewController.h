41e141345773736676ae07e68d6482ac9ffef6a5
razmashat <razmashat@gmail.com>
2018-03-04 15:19:06 +0200

    half way to support 10.0-11.2.6

--- unjailme/ViewController.h
+++ unjailme/ViewController.h
@@ -1,17 +1,9 @@
-//
-//  ViewController.h
-//  unjailme
-//
-//  Created by Sem Voigtländer on 27/02/2018.
-//  Copyright © 2018 Jailed Inc. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import <CoreBluetooth/CoreBluetooth.h>
 #import "exploit.h"
 @interface ViewController : UIViewController <CBCentralManagerDelegate>
 @property SandboxExploit* sbexploit;
+@property (weak, nonatomic) IBOutlet UITextField *numofports;
 @property BOOL bluetoothEnabled;
 @property CBCentralManager* bluetoothManager;
 @end
-
